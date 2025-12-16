558d5ba81b85ee893d9bbdcb0cd95ec1b37e6fb7
benasphy <binidani1903@gmail.com>
2025-12-16 11:02:58 +0300

    exercise

---
+++ exercise1.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.20;
+
+contract Coin{
+    address public minter;
+    mapping(address => uint) balances;
+
+    event Sent(address from, address to, uint amount);
+
+    constructor(){
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public{
+        require(msg.sender == minter, "Not a Minter");
+        balances[receiver] += amount;
+    }
+
+    error InsufficientBalance(uint requested, uint available);
+
+    function Send(address receiver, uint amount) public{
+        require(balances[msg.sender] >= amount, InsufficientBalance(amount, balances[msg.sender]));
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+
+        emit Sent(msg.sender, receiver, amount);
+    }
+
+}
